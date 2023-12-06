798c6052f6376d36ed59ce0471396eb48a100dbd
M514-T\admin <@>
2023-12-06 11:19:28 +0800

    安裝使用者身分驗證相關套件與功能

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*  路由名稱      所使用的URL               HTTP方法     所串接的控制器&方法
+products.index   Products                 GET           HEAD
+products.show    Products/{Product}       GET           HEAD
+products.create  Products/create          GET           HEAD
+products.store   Products                 POST          POST
+products.edit    Products/{Product}/edit  GET           HEAD
+products.update  Products/{Product}       PUT           PATCH
+products.destroy Products/{Product}       DELETE        DELETE
+                   各路由應該有的作用
+products.index     顯示所有products資料
+products.show      顯示單筆products資料
+products.create    指新增products頁面
+products.store     新增products頁面後儲存
+products.edit      編輯products頁面
+products.update    更新products資料
+products.destroy   刪除products資料
+*/
+/*
+Route::resource('Products', ProductController::class)->only([
+	'index','show','store','update','destroy'
+]);
+*/
+Route::get('Products', [ProductController::class, 'index']);
+Route::get('Products/create', [ProductController::class, 'create']);
+Route::get('Products/{Product}', [ProductController::class, 'show']);
+Route::post('Products', [ProductController::class, 'store']);
+Route::get('Products/{Product}/edit', [ProductController::class, 'edit']);
+Route::patch('Products/{Product}', [ProductController::class, 'update']);
+Route::delete('Products/{Product}', [ProductController::class, 'destroy']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
